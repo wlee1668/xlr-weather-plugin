f74392e54b608644722f55e41a7ca402fbc18afa
Wai Lee <wlee@xebialabs.com>
2018-10-05 16:35:29 -0400

    fixed build

--- src/main/resources/weather/task/check_weather.py
+++ src/main/resources/weather/task/check_weather.py
@@ -14,7 +14,7 @@ http_request = HttpRequest(params)
 weather_response = http_request.get('/data/2.5/weather?zip={},us&appid=926183652fba05ab14691aa5b6ed1faa'.format(zipcode))
 if weather_response.isSuccessful():
     json_data = json.loads(weather_response.getResponse())
-    temperature = json_data
+    temperature = json_data["main"]["temp"]
 else:
     sys.exit(1)
 
